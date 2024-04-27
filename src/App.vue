9f8b455ef195a4de9c6d841c88919fa1cb6ea969
Mateus Lima de Freitas <dev.org.ml@gmail.com>
2024-04-27 14:05:41 -0300

    Create app navigation and app bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
   <v-app>
+
+    <v-navigation-drawer v-model="isDrawerOpen">
+      <v-list>
+        <v-list-subheader>MENU</v-list-subheader>
+
+        <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+        <v-list-item prepend-icon="mdi-account">Usuários</v-list-item>
+        <v-list-item prepend-icon="mdi-clock">Horários</v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar flat>
+      <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>
+        Testing Vuetify 3
+      </v-app-bar-title>
+
+      
+    </v-app-bar>
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <h1>Dashboard</h1>
+        
+      </v-container>
     </v-main>
 
     <AppFooter />
@@ -9,5 +33,11 @@
 </template>
 
 <script setup>
-  //
+import { ref } from 'vue';
+
+  const isDrawerOpen = ref(false)
+
+  // return {
+  //   isDrawerOpen,
+  // }
 </script>
